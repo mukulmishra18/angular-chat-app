34efe456b3367313e3806c0db5db0ebf980d7b44
Mukul Mishra <mkmishra1895@gmail.com>
2018-04-13 02:14:22 +0530

    Adds new UI for chat app.

--- client/js/index.js
+++ client/js/index.js
@@ -3,15 +3,16 @@ var app = angular.module('myApp',[]);
 app.controller('myCtrl', function($scope){
 	var scoket = io.connect('http://localhost:8080');
   $scope.send = function () {
- 	  var msg = $scope.message;
+ 	 var msg = $scope.message;
     $scope.message = "";
 
     scoket.emit('message', msg);
  }
 
   scoket.on('message', function(message) {
+  	console.log(message);
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(message));
-    document.getElementById("messages").appendChild(li);
+    document.querySelector("ul").appendChild(li);
  });
 });
