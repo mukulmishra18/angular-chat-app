5d289d9690496b287797e968b593725ffaa8dc78
Mukul Mishra <mkmishra1895@gmail.com>
2018-04-17 01:37:12 +0530

    Adds normal message view

---
+++ client/index.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp',[]);
+
+app.controller('myCtrl', function($scope){
+	var scoket = io.connect('http://localhost:8080');
+  $scope.send = function () {
+ 	var msg = $scope.message;
+    $scope.message = "";
+
+    scoket.emit('message', msg);
+ }
+
+  scoket.on('message', function(message) {
+    var li = '<li style="width: 100%">' +
+      '<div class="macro flex-container">' +
+          '<img class="flex-img img-circle" src="client/img/bot.png"></img>' +
+          '<p> class="flex-text"' + message + '</p>' +
+      '</div>' +
+    '</li>';
+
+    $('ul').append(li);
+  });
+});
